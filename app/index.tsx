9cec0a6b33aab0290dccb86372be342455382c14
Muhammad Ali <alihere786@gmail.com>
2026-03-13 03:24:05 +0500

    refactor: remove unused components and setup NativeWind with Tailwind config

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-blue-100">
+      <Text className="text-4xl font-bold text-darkAccent">
+        Welcome to Nativewind!
+      </Text>
+    </View>
+  );
+}
